c80bc1e51faee31c989573171e96cc64bf75b943
CarloFanelli <carlo.fanelli.sax@gmail.com>
2023-09-26 18:04:43 +0200

    add selection for archetype and start to create selection

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,8 +1,14 @@
 <template>
     <main class="py-4">
-        <div class="container bg-white">
+        <div class="container">
+            <select name="cards_selection" id="cards_selections" :v-model="state.selection">
+                <option value="alien" selected>Alien</option>
+                <option value="Noble Knight">Noble Knigh</option>
+            </select>
+        </div>
+        <div class="container bg-white" v-if="state">
             <div class="row p-2">
-                <div class="my_col p-1" v-for=" card  in   state.cards  " v-show="card.archetype === 'Alien'">
+                <div class="my_col p-1" v-for=" card  in   state.cards  " v-show="card.archetype = 'Alien'">
                     <div class="card my-2 h-100">
                         <div class="card-header p-0">
                             <img class="w-100" :src='card.card_images[0].image_url' alt="">
@@ -17,6 +23,23 @@
                 </div>
             </div>
         </div>
+        <div class="container" v-else>
+            <div class="sk-circle">
+                <div class="sk-circle1 sk-child"></div>
+                <div class="sk-circle2 sk-child"></div>
+                <div class="sk-circle3 sk-child"></div>
+                <div class="sk-circle4 sk-child"></div>
+                <div class="sk-circle5 sk-child"></div>
+                <div class="sk-circle6 sk-child"></div>
+                <div class="sk-circle7 sk-child"></div>
+                <div class="sk-circle8 sk-child"></div>
+                <div class="sk-circle9 sk-child"></div>
+                <div class="sk-circle10 sk-child"></div>
+                <div class="sk-circle11 sk-child"></div>
+                <div class="sk-circle12 sk-child"></div>
+            </div>
+        </div>
+
     </main>
 </template>
 
@@ -31,12 +54,17 @@ export default {
     },
     created() {
         state.fetchData();
-    }
+    },
+    methods: {
+
+    },
 }
 </script>
 
 <style lang="scss" scoped>
 @use '../src/assets/scss/partials/variables' as *;
+@use '../assets/scss/partials/_loader.scss' as *;
+
 
 main {
     background: $yugioh__bg;
--- src/state.js
+++ src/state.js
@@ -2,17 +2,20 @@ import axios from 'axios';
 import { reactive } from 'vue';
 
 export const state = reactive({
-    base_url: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=1000&offset=0',
+    selection: '',
+    base_url: `https://db.ygoprodeck.com/api/v7/cardinfo.php?&num=1000&offset=0`,
     cards: null,
-    info: null,
 
     fetchData() {
         axios(this.base_url)
             .then(response => {
-                console.log(response.data);
-
+                // console.log(response.data);
+                console.log('selezione', this.selection);
                 this.cards = response.data.data;
 
             })
+            .catch(error => (
+                console.log('errore ', error)
+            ));
     }
 })
